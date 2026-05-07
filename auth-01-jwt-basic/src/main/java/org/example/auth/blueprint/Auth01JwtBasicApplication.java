cda4371229a3e596b89a9d2ba50c1e5a8cfb52f5
abdc06 <abdc2806@gmail.com>
2026-05-07 11:28:07 +0900

    최초 jwt-basic 프로젝트 생성

---
+++ auth-01-jwt-basic/src/main/java/org/example/auth/blueprint/Auth01JwtBasicApplication.java
@@ -0,0 +1,13 @@
+package org.example.auth.blueprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth01JwtBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Auth01JwtBasicApplication.class, args);
+    }
+
+}
