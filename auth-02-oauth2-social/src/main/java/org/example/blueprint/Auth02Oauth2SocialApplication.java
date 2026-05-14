d4395d91bea79cd9cf2401a07b08d595a17f4cf6
abdc06 <abdc2806@gmail.com>
2026-05-14 21:00:37 +0900

    auth-02-oauth2-social 생성

---
+++ auth-02-oauth2-social/src/main/java/org/example/blueprint/Auth02Oauth2SocialApplication.java
@@ -0,0 +1,13 @@
+package org.example.blueprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth02Oauth2SocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Auth02Oauth2SocialApplication.class, args);
+    }
+
+}
---
+++ auth-02-oauth2-social/src/test/java/org/example/blueprint/Auth02Oauth2SocialApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.blueprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth02Oauth2SocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
